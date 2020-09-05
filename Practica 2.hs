eb73b82e239b5f34a716b55541a60a951d7bf34d
Valentin Bonani <valentin.bonani@gmail.com>
2020-09-05 11:45:43 -0300

    Terminada Practica 2

--- Practica 2.hs
+++ Practica 2.hs
@@ -53,8 +53,8 @@ tts = (twice twice) swap
 
 -- Ejercicio 3
 
-const :: a -> (b -> a)
-const x = g
+const' :: a -> (b -> a)
+const' x = g
     where g y = x
 
 appDup :: ((a,a) -> c) -> (a -> c)
@@ -73,8 +73,8 @@ appDist :: (a -> b) -> ((a,a) -> (b,b))
 appDist f = g
     where g (x, y) = (f x, f y)
 
-flip :: (a -> (b -> c)) -> (b -> (a -> c))
-flip f = h 
+flip' :: (a -> (b -> c)) -> (b -> (a -> c))
+flip' f = h 
     where { h x = k
         where k y = (f y) x }
                      
@@ -134,4 +134,49 @@ f' = g
 g :: a -> Bool
 g x = False
 g' = \x -> True == False
+
+-- Ejercicio 6
+
+a6 = \p -> let (f, g) = p
+    in \x -> (f x, g x) 
+
+a6R = a6 (second,first) (2,3) == appFork (second,first) (2,3) {- Que devuelve true -}
+
+
+b6 :: (t1 -> t2 -> t3) -> (t1 -> t2) -> t1 -> t3
+b6 = \f -> \g -> \x -> (f x) (g x)
+
+b6R = b6 (*) doble 6 == subst (*) doble 6 {- Que devuelve true -}
+
+
+c6 = \f -> \x -> \y -> (f y) x
+c6R = c6 (/) 1 2 == flip' (/) 1 2 {- Que devuelve true -}
+
+d6 = \f -> \t -> let (x, y) = t
+    in (f x, f y)
+
+d6R = d6 succ (2,3) == appDist succ (2,3) {- Que devuelve true -}
+
+e6 = \x -> \y -> x
+e6R = e6 1 2 == const' 1 2
+
+f6 = \pf -> let (f, g) = pf
+    in \px -> let (x, y) = px
+        in (f x, g y)
+
+f6R = f6 (succ,doble) (3,5) == appPar (succ,doble) (3,5) {- Que devuelve true -}
+ 
+
+g6 =  \f -> (\x -> f (x, x))
+
+g6R = g6 first 1 == appDup first 1 {- Que devuelve true -}
+
+-- Ejercicio 7
+
+a7 = appFork (id,id) {-  :: \x -> (x,x) == c7 -}
+b7 =  \f -> appDup (appDist f) {- ::\f -> \x -> (f x,f x) == d7  -}
+c7 = appDup id {-  :: \x -> (x,x) == a7 -}
+d7 =  appDup appFork {- :: \f -> \x -> (f x, f x) == b7 -}
+e7 = flip (appDup const) {- :: \x -> \y -> (y,y) == f7 -}
+f7 = const (appDup id) {- :: \x -> \y -> (y,y) == e7  -}
 
